ddaf18168638551f3ec709aa27e5f8c2a4dab981
wunmib <bonojowunmi@gmail.com>
2023-01-04 18:46:07 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ['first_name']
+"""This defines a locked class"""
 
-    def __init__(self, first_name=""):
-        self.first_name = first_name
+
+class LockedClass:
+    """
+    Only allows instatiation of an attribute called first_name
+    """
+
+    __slots__ = ["first_name"]
